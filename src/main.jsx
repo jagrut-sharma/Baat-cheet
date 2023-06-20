e69cf5fe53af1176a0ae7a8ad5e054f543ccdcab
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2023-06-20 13:11:49 +0530

    :truck: refactored repo

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { makeServer } from "./server";
-
-// Call make Server
-makeServer();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
